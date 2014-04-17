8184ef2abb2b5e673992804b2b1a70f1255bcae1
Hugh Greenish <github@monochromechameleon.com>
2014-04-17 17:14:37 +0100

    further cleanup

--- src/MovieRatings.java
+++ src/MovieRatings.java
@@ -1,49 +1,37 @@
 
-import customwritables.*;
 import java.util.Arrays;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MovieRatings {
 
-    public static void runJob(String[] input, String output) throws Exception {
-
+    public static void runJob(String input, String output, MovieJob jobType) throws Exception {
         Configuration conf = new Configuration();
         Job job = new Job(conf);
-        //config
+        
         job.setJarByClass(MovieRatings.class);
-        job.setReducerClass(MovieCombiner.class);
-        job.setCombinerClass(MovieCombiner.class);
-        job.setMapperClass(MovieMapper.class);
-
-        //input format
-        job.setInputFormatClass(TextInputFormat.class);
-        //job.setNumReduceTasks(3);
-
-        //reducer output
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(MovieAndTags.class);
-
-        //mapper output
-        job.setMapOutputKeyClass(IntWritable.class);
-        job.setMapOutputValueClass(MovieAndTags.class);
-
+        
+        jobType.configureJob(job);
+        
         Path outputPath = new Path(output);
 
-        FileInputFormat.setInputPaths(job, StringUtils.join(input, ","));
+        FileInputFormat.setInputPaths(job, input);
         FileOutputFormat.setOutputPath(job, outputPath);
         outputPath.getFileSystem(conf).delete(outputPath, true);
         job.waitForCompletion(true);
     }
 
     public static void main(String[] args) throws Exception {
-        runJob(Arrays.copyOfRange(args, 0, args.length - 1), args[args.length - 1]);
+        String[] inputPaths = Arrays.copyOfRange(args, 0, args.length - 1);
+        String outputPath = args[args.length - 1];
+        
+        String joinedInput = StringUtils.join(inputPaths, ",");
+        runJob(joinedInput, MovieJob.TagCount.getOutputFileName(), MovieJob.TagCount);
+        runJob(MovieJob.TagCount.getOutputFileName(), MovieJob.MoviePairs.getOutputFileName(), MovieJob.MoviePairs);
+        runJob(MovieJob.MoviePairs.getOutputFileName(), outputPath, MovieJob.Similarity);
     }
 }
